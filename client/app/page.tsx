62a507f035b341795ffe39d9de341c2ba0071310
hakim <abdelhakimdehina10@gmail.com>
2026-04-30 13:33:45 +0100

    Update About component and add FAQ component with accordion UI

--- client/app/page.tsx
+++ client/app/page.tsx
@@ -1,11 +1,13 @@
 import Image from "next/image";
-
+import AboutTeamSection from "@/components/localComps/About";
+import { SentimentAnalysisFAQ } from "@/components/localComps/FAQ";
 export default function Home() {
   return (
     <div className="flex flex-col flex-1 items-center justify-center bg-zinc-50 font-sans dark:bg-black">
       <main>
         <div>
-          
+          <AboutTeamSection/>
+          <SentimentAnalysisFAQ />
         </div>
       </main>
     </div>
