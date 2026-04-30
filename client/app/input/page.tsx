632b835578de9dce2bd74054ffd567527f93b307
oxilonx <boabmeuni01@gmail.com>
2026-04-30 09:35:11 +0100

    feat:structure prj init

---
+++ client/app/input/page.tsx
@@ -0,0 +1,6 @@
+import Image from "next/image";
+const page = () => {
+  return <div className="">this is input pag</div>;
+};
+
+export default page;
---
+++ client/app/input/popo/page.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const page = () => {
+  return <div>asdas</div>;
+};
+
+export default page;
